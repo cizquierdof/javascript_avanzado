a152f29d2a3b26e32faef0327aa8c958be6fc291
cizquierdof <cizquierdof@gmail.com>
2020-03-28 20:17:25 +0100

    estilos

--- 07-ejercicio-dom/cargar.js
+++ 07-ejercicio-dom/cargar.js
@@ -2,83 +2,97 @@
 //se debe importar sin llaves
 import pizzas from './pizzas-content.js';
 
-/* pizzas.forEach(
-    (pizza) => {
-    console.log(pizza);
-});
- */
+const createTable = () => {
 
-const elemento = () => {
-    const a = document.createElement("a");
-    a.href = "www.google.com";
-    a.innerText="Mi enlace";
-    return a;
-}
+    /*****************
+    *creo la tabla y le añado un id que luego utilizaremos para localizarla
+    *************/
+    const tabla = document.createElement("table");
+    tabla.setAttribute('id', 'tabla_pizzas');
+    tabla.setAttribute('class','sortable');
 
-const createTable=()=>{
-    const tabla=document.createElement("table");
-    tabla.setAttribute('class','table');
-    //tabla.style.border='solid';
-
-    const divTabla=document.getElementById("tabla");
+    //meto la clase en el div
+    const divTabla = document.getElementById("tabla");
     divTabla.appendChild(tabla);
 
+    //llamamos a los elementos de la cabecera y se los metemos en la tabla
     tabla.appendChild(_headerTable());
 
-    
-
-    _createTableRow(pizzas).forEach(element=>{
+    //recorremos el array creado por _createTableRow y lo añadimos como elementos
+    _createTableRow(pizzas).forEach(element => {
         tabla.appendChild(element)
     });
 
+    return tabla;
+
 }
 
-const _headerTable=()=>{ 
+const _headerTable = () => {
     //crea los elementos que froman la cabecera   
-    const thead=document.createElement('thead');    //<thead></thead>
-    const tr_header=document.createElement("tr");   //<tr></tr>
-    const th_id=document.createElement("th");       //<th>id</th>
-    th_id.innerText='id';
-    const th_nombre=document.createElement("th");   //<th>Nombre</th>
-    th_nombre.innerText='nombre';
+    const thead = document.createElement('thead');    //<thead></thead>
+    const tr_header = document.createElement("tr");   //<tr></tr>
+    const th_id = document.createElement("th");       //<th>id</th>
+    th_id.innerText = 'Id';
+    const th_nombre = document.createElement("th");   //<th>Nombre</th>
+    th_nombre.innerText = 'Nombre';
+
     //mete los elementos en sus correspondientes padres
     thead.appendChild(tr_header);
     tr_header.appendChild(th_id);
     tr_header.appendChild(th_nombre);
-    //crea los estilos de visualización
-    thead.style.cssText='background: #0B6FA4; border-bottom: 5px solid #FFFFFF; ';
-    th_id.style.cssText='font-size: 17px; font-weight: bold; color: #FFFFFF;';
-    th_nombre.style.cssText='font-size: 17px; font-weight: bold; color: #FFFFFF;';
+    //crea los estilos de visualización de la cabecera de la tabla
+    thead.style.cssText = 'background: #0B6FA4; border-bottom: 5px solid #FFFFFF;'
+        + ' font-weight: bold; font-size: 17px;  color: #FFFFFF;';
 
     //devuelve la cabecera completa
     return thead;
 }
 
- const _createTableRow=(pizzas)=>{
-    const arrayrows=[];
-    let row=1;
-    pizzas.forEach(element => {
-        const tr=document.createElement("tr");
+const _createTableRow = (pizzas) => {
+    const arrayrows = [];
+    let trBackgroundColor;
 
-        const tdId=document.createElement("td");
-        tdId.innerText=element.id;
-        const tdNombre=document.createElement("td");
-        tdNombre.innerText=element.nombre;
+    pizzas.forEach(element => {
+
+        /**************
+        *crear elementos para cada línea de pizza y popularlos
+        **************/
+        const tr = document.createElement("tr");
+        const tdId = document.createElement("td");    //id
+        tdId.innerText = element.id;
+        const tdNombre = document.createElement("td");    //nombre
+        tdNombre.innerText = element.nombre;
+        //insertar en la row
         tr.appendChild(tdId);
         tr.appendChild(tdNombre);
-        tr.style.
-        arrayrows.push(tr);
-        if(row%2==0){
-            tr.style.background='#d0e4f5';
-        }
-        row++;
+
+        /*************
+         * Estilos de la fila
+         */
+        tr.style.cssText = 'font-size:17px; height: 1.5em;';
+        tdId.style.padding = '5px';
+        tdNombre.style.padding = '5px';
+
+
+        /*************
+        *añadir row al array de filas y obtener la longitud del array
+        *************/
+        let row = arrayrows.push(tr);
+
+        //gackground filas pares e impares en base a la actual longitud del array
+        row % 2 ? trBackgroundColor = '#ffffff' : trBackgroundColor = '#d0e4f5';
+        tr.style.background = trBackgroundColor;
+
 
     });
-    return arrayrows;
- }
+    return arrayrows;   //devolvemos un array de filas de datos para la tabla
+}
 
+
+
+//parámetro que enviamos a init para que empiece a renderizar elementos
 const renderElement = () => {
-createTable();
+    createTable();
 }
 
 export const init = () => {
