f20de986c02886162f672bd59a871119356e0c56
cizquierdof <cizquierdof@gmail.com>
2020-04-01 12:27:33 +0200

    .

--- 07-ejercicio-dom/mi_solucion/modulos.js
+++ 07-ejercicio-dom/mi_solucion/modulos.js
@@ -5,14 +5,14 @@ import pizzas from './pizzas-content.js';
 const _ordenaPizzasPorCampo = (campo) => {
     let pizzaOrdenada = [];
 
-     if (campo == 'id') {
+    if (campo == 'id') {
         pizzaOrdenada = pizzas.sort(
             (a, b) => {
                 if (a.id > b.id) { return 1; }
                 if (a.id < b.id) { return -1; }
                 return 0;
             });
-    }  else if (campo = 'nombre') {
+    } else if (campo = 'nombre') {
         pizzaOrdenada = pizzas.sort(
             (a, b) => {
                 if (a.nombre > b.nombre) { return 1; }
@@ -53,7 +53,7 @@ const _headerTable = () => {
     //crea los elementos que froman la cabecera   
     const thead = document.createElement('thead');    //<thead></thead>
     const tr_header = document.createElement("tr");   //<tr></tr>
-    tr_header.id='tr_header';
+    tr_header.id = 'tr_header';
     const th_id = document.createElement("th");       //<th>id</th>
     th_id.innerText = 'Id';
 
@@ -85,6 +85,41 @@ const _headerTable = () => {
     return thead;
 }
 
+const _muestraDetallesPizza = (pizza) => {
+    console.log(pizza);
+
+    const div_detalle = document.getElementById('ingredientes');
+
+    div_detalle.innerHTML='';
+
+    const h_nombre = document.createElement('h2');
+    h_nombre.innerHTML = pizza.nombre;
+    div_detalle.appendChild(h_nombre);
+
+    const ul_ingredientes=document.createElement('ul');
+    div_detalle.appendChild(ul_ingredientes);
+
+    pizza.listaIngredientes.forEach(element=>{
+        const li_ingrediente=document.createElement('li');
+        li_ingrediente.innerText=element.nombre;
+        ul_ingredientes.appendChild(li_ingrediente);
+    })
+
+    const div_comentarios=document.getElementById('comentarios');
+    div_comentarios.innerHTML='';
+    
+    pizza.listaComentarios.forEach(element=>{
+        const div_linea_comentario=document.createElement('div');
+
+        div_linea_comentario.innerText=`${element.fecha}`element.texto;
+        div_linea_comentario.className='linea_comentario'
+        div_comentarios.appendChild(div_linea_comentario);
+
+    });    
+
+
+}
+
 const _createTableRow = (pizzas) => {
     const arrayrows = [];
     let trBackgroundColor;
@@ -103,20 +138,21 @@ const _createTableRow = (pizzas) => {
         tr.appendChild(tdId);
         tr.appendChild(tdNombre);
 
+        tr.addEventListener('click',
+            () => { _muestraDetallesPizza(pizzas[row - 1]); }
+        )
+
         /*************
         *añadir row al array de filas y obtener la longitud del array
         *************/
         let row = arrayrows.push(tr);
-        if(!(row%2)){
-            tr.className='impar';
+        if (!(row % 2)) {
+            tr.className = 'impar';
         }
-        //gackground filas pares e impares en base a la actual longitud del array
-       // row % 2 ? trBackgroundColor = '#ffffff' : trBackgroundColor = '#d0e4f5';
-        //tr.style.background = trBackgroundColor;
 
     });
     return arrayrows;   //devolvemos un array de filas de datos para la tabla
 }
 
-const init=()=>{createTable();}
+const init = () => { createTable(); }
 document.body.addEventListener('load', init());
